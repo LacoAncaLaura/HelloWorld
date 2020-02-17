114cc0cda1e5b07ef9727d8e455aa8e9703b7123
Barta Iosif <iosif.barta@gmail.com>
2020-02-17 21:01:13 +0200

    some changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
       public static void main (String[] args) {
-             System.out.println("Hello World!We are learning GIT");
+             System.out.println("Hello World!We are learning GIT in 2");
     }
 }
